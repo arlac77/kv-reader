f9c4a9ba3e18b26b7012ebc78d257aba09005fbc
Markus Felten <Markus.Felten@gmx.de>
2018-07-16 22:44:56 +0200

    chore(package): add rollup-plugin-istanbul@^2.0.1 chore(rollup): import istanbul chore(rollup): add istanbul to plugins

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,3 +1,5 @@
+import istanbul from 'rollup-plugin-istanbul';
+
 import multiEntry from 'rollup-plugin-multi-entry';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
@@ -5,7 +7,9 @@ import commonjs from 'rollup-plugin-commonjs';
 export default {
   input: 'tests/**/*-test.js',
   external: ['ava', 'fs', 'path'],
-  plugins: [multiEntry(), resolve(), commonjs()],
+  plugins: [multiEntry(), resolve(), commonjs(), istanbul({
+    exclude: ['tests/**/*-test.js']
+  })],
 
   output: {
     file: 'build/bundle-test.js',
