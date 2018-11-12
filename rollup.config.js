d6b508fd866f87cba3c3204efe379126154c3e5f
Markus Felten <Markus.Felten@gmx.de>
2018-11-12 23:56:27 +0100

    fix: get rid of babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import json from "rollup-plugin-json";
 import cleanup from 'rollup-plugin-cleanup';
 import executable from 'rollup-plugin-executable';
 import pkg from './package.json';
@@ -6,7 +5,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-  plugins: [resolve(), commonjs()],
+  plugins: [resolve(), commonjs(), cleanup()],
 
   output: {
     file: pkg.main,
